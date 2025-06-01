b5574888761c58c950d796224f1bde0855c5e472
Muhammad Fadhilah <muhammadfadhilah443@gmail.com>
2025-06-01 13:47:54 +0700

    Init repository.

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Typed from "typed.js";
+
+const myTyped = new Typed("#typed", {
+  strings: [
+    "Student of Devscale Indonesia's Full-Stack JavaScript Bootcamp Batch 9.",
+    "Anime Enthusiast.",
+    "Visual Novel Enthusiast.",
+  ],
+  loop: true,
+  typeSpeed: 25,
+});
